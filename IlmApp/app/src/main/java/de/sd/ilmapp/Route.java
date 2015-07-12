eced0bd11d2ef4cd1069047842a22e4f6c7ebc2d
astheny <heyder.stefan@googlemail.com>
2015-07-12 18:22:47 +0200

    added necessary methods

--- IlmApp/app/src/main/java/de/sd/ilmapp/Route.java
+++ IlmApp/app/src/main/java/de/sd/ilmapp/Route.java
@@ -1,9 +1,34 @@
 package de.sd.ilmapp;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sebastian on 04.07.2015.
  */
 public class Route {
     String type;
-    Point[] stations;
+    ArrayList<Point> stations;
+
+    public Route(String type, ArrayList<Point>  stations) {
+        this.type = type;
+        this.stations = stations;
+    }
+
+    // TODO: Change method signature to fit into Google Maps API
+    public String calculateRoute() {
+        return null;
+    }
+
+    // TODO: Add Check for types?
+    public void addLocationAtLastPosition(Point location) {
+        stations.add(location);
+    }
+
+    // TODO: Add Check for types?
+    public void addLocationAtFirstPosition(Point location){
+        stations.add(location);
+    }
+
+
+
 }
